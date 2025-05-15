1675654892004bc43905f182fee8e17763d89c76
mario ngola <mariongola@email.com>
2025-05-15 18:22:54 +0100

    first angular pj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,22 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo-app';
+  {
+
+  tarefa : String = "";
+  tarefas : Array<String> = [
+    "Tarefa 1",
+    "Tarefa 2",
+    "Tarefa 3",
+    "Tarefa 4",
+    "Tarefa 5"
+  ];
+
+  adicionar() {
+    this.tarefas.push(this.tarefa);
+    this.tarefa = "";
+  }
+
 }
+
+
